2c6544013f957285b78c2bcc22e265255267bc40
Fernando Hernandez <fernando.hernandez@globalesm.com>
2021-09-06 09:03:58 -0700

    Update gitignore file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const createError = require('http-errors')
 const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
+// const expressLayouts = require('express-ejs-layouts')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
