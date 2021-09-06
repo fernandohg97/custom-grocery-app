2b4314548dbea3a4e4e8ca03a354a33c7371f22f
Fernando Hernandez <fernando.hernandez@globalesm.com>
2021-09-06 11:57:18 -0700

    Update responsive styles for product price and price checker views

---
+++ routes/offers.route.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const offerRouter = express.Router()
+const offerCtrl = require('../controllers/offers.controller')
+// const validateProduct = require('../middlewares/productValidation')
+
+// Route Views
+
+module.exports = offerRouter
