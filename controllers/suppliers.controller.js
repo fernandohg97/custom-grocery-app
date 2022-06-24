0b9a4edda2f1f38ec5ac2a76520b5ceebdb9fdb8
Fernando Hernandez <fernando.hernandez@globalesm.com>
2022-06-24 13:29:22 -0700

    Update providers list

--- controllers/suppliers.controller.js
+++ controllers/suppliers.controller.js
@@ -4,15 +4,13 @@ const url = require('url')
 const axios = require('axios').default
 const createError = require('http-errors')
 const { google } = require('googleapis')
-const authSheets = require('../middlewares/authSheets')
+// const authSheets = require('../middlewares/authSheets')
 
 class Supplier {
   static getAllUrl = 'https://api.hikeup.com/api/v1/suppliers/get_all'
   static getAllProductsUrl = 'https://api.hikeup.com/api/v1/products/get_all'
   static suppliersLimit = 200
 
-  // SPREADSHEET ID OF LISTA DE PRODUCTOS
-  static spreadsheetId = '1G2_8KuVDJwXBo7fUuOni2y7FWVU-Zc_vTO8VnUprO1s'
 
   static async allSuppliers(req, res, next) {
     // Options for this request
@@ -30,6 +28,28 @@ class Supplier {
         suppliers: suppliers.items,
         totalCount
       })
+    } catch (error) {
+      console.log(error)
+      const { response } = error
+      console.log(response)
+      if (response.status === 400) return next(createError(400, 'Peticion incorrecta'))
+      return next(error)
+    }
+  }
+
+  static async suppliersList(req, res, next) {
+    // Options for this request
+    // const opts = { headers: { accept: 'application/json', Authorization: `Bearer ${req.session.accessToken}` } }
+    // const searchFilter = 'CS0'
+    try {
+      // // Call API to get all products
+      // const data = await axios.get(`${Supplier.getAllUrl}?page_size=${Supplier.suppliersLimit}&Skip_count=${0}`, opts)
+
+      // const suppliers = data.data
+      // const totalCount = suppliers.totalCount
+
+      // console.log(totalCount, numberOfPages, suppliers.items.length)
+      return res.render('pages/suppliers/suppliers')
     } catch (error) {
       console.log(error)
       const { response } = error
@@ -81,20 +101,69 @@ class Supplier {
     }
   }
 
-  // static async updateProductData(req, res, next) {
-  //   // const data = req.body
-  //   const { supplierId } = req.query
+  // static async updateProducts(req, res, next) {
+  //   const resultsPerPage = 1500
+  //   const skipCount = 0
+  //   const { supplierId, supplierName } = req.query
+
+  //   // Get auth client
+  //   const authClient = await authSheets()
+
+  //   // Instance of google sheets api
+  //   const googleSheets = google.sheets({ version: 'v4', auth: authClient})
+
+  //   // Sheetname and range to be updated
+  //   const sheetName = supplierName
+  //   const range = '!D2:L'
+
+  //   // Set options to send request
+  //   let options = {
+  //     auth: authClient,
+  //     spreadsheetId: Supplier.spreadsheetId,
+  //     range: sheetName + range,
+  //     resource: {
+  //       values: undefined
+  //     },
+  //     valueInputOption: 'USER_ENTERED',
+  //   }
+
+  //   // Get products from Hike API
   //   const sorting = 'name'
   //   const opts = { headers: { accept: 'application/json', Authorization: `Bearer ${req.session.accessToken}` } }
   //   // const searchFilter = 'CS0'
   //   try {
   //     // Call API to get all products
-  //     const data = await axios.get(`${Supplier.getAllProductsUrl}?page_size=${res.locals.resultsPerPage}&Skip_count=${res.locals.skipCount}&sorting=${sorting}&supplierIds=${supplierId}`, opts)
+  //     const data = await axios.get(`${Supplier.getAllProductsUrl}?page_size=${resultsPerPage}&Skip_count=${skipCount}&sorting=${sorting}&supplierIds=${supplierId}`, opts)
 
   //     const products = data.data
   //     // Omit parent product line
-  //     // const filterProducts = products.items.filter(product => !product.has_variants)
-  //     return res.status(201).send({message: 'Products successfully uploaded!'})
+  //     const filterProducts = products.items.reduce((names, product) => {
+  //       if (!product.has_variants) {
+  //         names.push([
+  //           product.name,
+  //           product.sku, 
+  //           product.barcode, 
+  //           product.product_type.length > 0 ? product.product_type[0].type_name : 'n/a', 
+  //           product.product_suppliers.length > 0 ? product.product_suppliers.map(supp => supp.supplier_name).join(',') : 'n/a',
+  //           product.product_outlets.length > 0 ? product.product_outlets[0].tax_name : 'n/a',
+  //           product.product_outlets.length > 0 ? product.product_outlets[0].cost_price : 'n/a',
+  //           product.product_outlets.length > 0 ? product.product_outlets[0].price_inc_tax : 'n/a',
+  //           product.product_outlets.length > 0 ? product.product_outlets[0].price_ex_tax : 'n/a'
+  //         ])
+  //       }
+  //       return names
+  //     }, [])
+
+  //     options.resource.values = filterProducts
+
+  //     // Read rows from spreadsheet
+  //     const response = await googleSheets.spreadsheets.values.update(options);
+
+  //     return res.render('pages/suppliers/price-list', {
+  //       supplierName,
+  //       supplierId,
+  //       successMessage: `Se actualizaron ${response.data.updatedRows} productos exitosamente!`, 
+  //     })
   //   } catch (error) {
   //     console.log(error)
   //     const { response } = error
@@ -103,78 +172,6 @@ class Supplier {
   //     return next(error)
   //   }
   // }
-
-  static async updateProducts(req, res, next) {
-    const resultsPerPage = 1500
-    const skipCount = 0
-    const { supplierId, supplierName } = req.query
-
-    // Get auth client
-    const authClient = await authSheets()
-
-    // Instance of google sheets api
-    const googleSheets = google.sheets({ version: 'v4', auth: authClient})
-
-    // Sheetname and range to be updated
-    const sheetName = supplierName
-    const range = '!D2:L'
-
-    // Set options to send request
-    let options = {
-      auth: authClient,
-      spreadsheetId: Supplier.spreadsheetId,
-      range: sheetName + range,
-      resource: {
-        values: undefined
-      },
-      valueInputOption: 'USER_ENTERED',
-    }
-
-    // Get products from Hike API
-    const sorting = 'name'
-    const opts = { headers: { accept: 'application/json', Authorization: `Bearer ${req.session.accessToken}` } }
-    // const searchFilter = 'CS0'
-    try {
-      // Call API to get all products
-      const data = await axios.get(`${Supplier.getAllProductsUrl}?page_size=${resultsPerPage}&Skip_count=${skipCount}&sorting=${sorting}&supplierIds=${supplierId}`, opts)
-
-      const products = data.data
-      // Omit parent product line
-      const filterProducts = products.items.reduce((names, product) => {
-        if (!product.has_variants) {
-          names.push([
-            product.name,
-            product.sku, 
-            product.barcode, 
-            product.product_type.length > 0 ? product.product_type[0].type_name : 'n/a', 
-            product.product_suppliers.length > 0 ? product.product_suppliers.map(supp => supp.supplier_name).join(',') : 'n/a',
-            product.product_outlets.length > 0 ? product.product_outlets[0].tax_name : 'n/a',
-            product.product_outlets.length > 0 ? product.product_outlets[0].cost_price : 'n/a',
-            product.product_outlets.length > 0 ? product.product_outlets[0].price_inc_tax : 'n/a',
-            product.product_outlets.length > 0 ? product.product_outlets[0].price_ex_tax : 'n/a'
-          ])
-        }
-        return names
-      }, [])
-
-      options.resource.values = filterProducts
-
-      // Read rows from spreadsheet
-      const response = await googleSheets.spreadsheets.values.update(options);
-
-      return res.render('pages/suppliers/price-list', {
-        supplierName,
-        supplierId,
-        successMessage: `Se actualizaron ${response.data.updatedRows} productos exitosamente!`, 
-      })
-    } catch (error) {
-      console.log(error)
-      const { response } = error
-      console.log(response)
-      if (response.status === 400) return next(createError(400, 'Peticion incorrecta'))
-      return next(error)
-    }
-  }
 }
 
 module.exports = Supplier
--- routes/suppliers.route.js
+++ routes/suppliers.route.js
@@ -5,9 +5,10 @@ const { getAllSupplierProductPageParams } = require('../middlewares/getPageParam
 
 // Route Views
 /* GET all cost and retail price for all products. */
-supplierRouter.get('/', supplierCtrl.allSuppliers)
+supplierRouter.get('/', supplierCtrl.allSuppliers) // Supplier checker
 supplierRouter.get('/product-list', supplierCtrl.allProductsBySupplier)
+supplierRouter.get('/list', supplierCtrl.suppliersList)
 // supplierRouter.get('/update-product-data', getAllSupplierProductPageParams, supplierCtrl.updateProductData)
-supplierRouter.get('/sheets/update-product-data', supplierCtrl.updateProducts)
+// supplierRouter.get('/sheets/update-product-data', supplierCtrl.updateProducts)
 
 module.exports = supplierRouter
